5083835bf7e88611a7b75c5a3e6122c4ec7ffe78
fahd114 <fahd.320250114@ejust.edu.eg>
2026-04-30 16:22:04 +0300

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,23 +1,55 @@
 #include "Customer.h"
-
-using namespace std;
+#include <iostream>
 
 Customer::Customer(int id, int arrTime, int transTime, bool vip) {
     this->id = id;
-    arrivalTime = arrTime;
-    transactionTime = transTime;
-    waitingTime = 0;
-    isVIP = vip;
-    status = WAITING;
+    this->arrivalTime = arrTime;
+    this->transactionTime = transTime;
+    this->isVIP = vip;
+    this->waitingTime = 0;
+    this->status = WAITING;
 }
 
-int Customer::getId() const { return id; }
-int Customer::getArrivalTime() const { return arrivalTime; }
-int Customer::getTransactionTime() const { return transactionTime; }
-int Customer::getWaitingTime() const { return waitingTime; }
-bool Customer::getIsVIP() const { return isVIP; }
-Status Customer::getStatus() const { return status; }
+int Customer::getId() const {
+    return id;
+}
 
-void Customer::setWaitingTime(int time) { waitingTime = time; }
-void Customer::incrementWaitingTime() { waitingTime++; }
-void Customer::setStatus(Status s) { status = s; }
+int Customer::getArrivalTime() const {
+    return arrivalTime;
+}
+
+int Customer::getTransactionTime() const {
+    return transactionTime;
+}
+
+int Customer::getWaitingTime() const {
+    return waitingTime;
+}
+
+bool Customer::getIsVIP() const {
+    return isVIP;
+}
+
+Status Customer::getStatus() const {
+    return status;
+}
+
+void Customer::setWaitingTime(int time) {
+    waitingTime = time;
+}
+
+void Customer::calculateWaitingTime(int currentTime) {
+    waitingTime = currentTime - arrivalTime;
+}
+
+void Customer::setStatus(Status s) {
+    status = s;
+}
+
+void Customer::printInfo() const {
+    std::cout << "Customer ID: " << id 
+              << " | Arrival: " << arrivalTime 
+              << " | Transaction: " << transactionTime 
+              << " | VIP: " << (isVIP ? "Yes" : "No") 
+              << " | Waiting Time: " << waitingTime << std::endl;
+}
