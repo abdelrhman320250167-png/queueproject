8214b2d70ca7b17bd52156cc43788af7ebda5c33
fahd114 <fahd.320250114@ejust.edu.eg>
2026-04-30 16:01:13 +0300

    Update server.h

--- server.h
+++ server.h
@@ -1,20 +1,24 @@
 #ifndef SERVER_H
 #define SERVER_H
+
 #include "Customer.h"
 
 class Server {
 private:
-    Customer currentCustomer;
+    Customer* currentCustomer;
     bool busy;
     int remainingTime;
 
 public:
     Server();
+
     bool isFree() const;
-    void assignCustomer(Customer c);
-    void updateTime(); 
+
+    void assignCustomer(Customer* c);
+    void updateTime();
     void finishService();
-    Customer getCurrentCustomer() const;
+
+    Customer* getCurrentCustomer() const;
 };
 
 #endif
