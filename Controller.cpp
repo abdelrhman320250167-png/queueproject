ae68d149867d51dc113c36a2063fd7f9fb13bcbb
Alaa-alkady <alaa.320250172@ejust.edu.eg>
2026-05-08 17:44:23 +0300

    Controller.cpp

--- Controller.cpp
+++ Controller.cpp
@@ -1,24 +1,22 @@
 #include "Controller.h"
 #include <SFML/System.hpp>
 
-// إعطاء قيم ابتدائية للمحاكاة (مثلا 5 سيرفرات و 500 وقت)
 Controller::Controller() : sim(5, 500) {
 }
 
 void Controller::initializeSystem() {
-    // الواجهة والمحاكاة بيتعملهم Initialize تلقائياً دلوقتي
 }
 
 void Controller::startSimulation() {
     sf::Clock clock;
-    float tickRate = 0.5f; // سرعة المحاكاة: كل نص ثانية 
+    float tickRate = 0.5f;   
 
     while (gui.isOpen()) {
         
         gui.handleEvents(); 
 
         if (sim.getIsRunning()) {
-            // تحديث المحاكاة كل نص ثانية
+        
             if (clock.getElapsedTime().asSeconds() >= tickRate) {
                 updateSystem();
                 clock.restart(); 
