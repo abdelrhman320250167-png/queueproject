89c9fcb6d402e3ec390db55ea4904abfee3c60df
Alaa-alkady <alaa.320250172@ejust.edu.eg>
2026-05-08 16:57:14 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 int main() {
     Controller controller;
+    
+    // استدعاء دالة التهيئة ثم بدء المحاكاة
+    controller.initializeSystem();
     controller.startSimulation();
     
     return 0;
