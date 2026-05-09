3fed1d96dc5e21842ccd9ae4bda85c67fe1a3127
fahd114 <fahd.320250114@ejust.edu.eg>
2026-05-09 14:57:14 +0300

    Update customer.h

--- customer.h
+++ customer.h
@@ -14,14 +14,12 @@ private:
 
 public:
     Customer(int id = 0, int arrTime = 0, int transTime = 0, bool vip = false);
-
     int getId() const;
     int getArrivalTime() const;
     int getTransactionTime() const;
     int getWaitingTime() const;
     bool getIsVIP() const;
     Status getStatus() const;
-
     void setWaitingTime(int time);
     void calculateWaitingTime(int currentTime);
     void setStatus(Status s);
