dabcd862767b0e2a7e9859cdc67562b4aadfa5a2
fahd114 <fahd.320250114@ejust.edu.eg>
2026-05-09 14:58:22 +0300

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,45 +1,32 @@
 #include "Customer.h"
 #include <iostream>
 
+using namespace std;
+
 Customer::Customer(int id, int arrTime, int transTime, bool vip) {
     this->id = id;
     this->arrivalTime = arrTime;
     this->transactionTime = transTime;
-    this->isVIP = vip;
     this->waitingTime = 0;
+    this->isVIP = vip;
     this->status = WAITING;
 }
 
-int Customer::getId() const {
-    return id;
-}
-
-int Customer::getArrivalTime() const {
-    return arrivalTime;
-}
-
-int Customer::getTransactionTime() const {
-    return transactionTime;
-}
-
-int Customer::getWaitingTime() const {
-    return waitingTime;
-}
-
-bool Customer::getIsVIP() const {
-    return isVIP;
-}
-
-Status Customer::getStatus() const {
-    return status;
-}
+int Customer::getId() const { return id; }
+int Customer::getArrivalTime() const { return arrivalTime; }
+int Customer::getTransactionTime() const { return transactionTime; }
+int Customer::getWaitingTime() const { return waitingTime; }
+bool Customer::getIsVIP() const { return isVIP; }
+Status Customer::getStatus() const { return status; }
 
 void Customer::setWaitingTime(int time) {
     waitingTime = time;
 }
 
 void Customer::calculateWaitingTime(int currentTime) {
-    waitingTime = currentTime - arrivalTime;
+    if (status == WAITING) {
+        waitingTime = currentTime - arrivalTime;
+    }
 }
 
 void Customer::setStatus(Status s) {
@@ -47,9 +34,7 @@ void Customer::setStatus(Status s) {
 }
 
 void Customer::printInfo() const {
-    std::cout << "Customer ID: " << id 
-              << " | Arrival: " << arrivalTime 
-              << " | Transaction: " << transactionTime 
-              << " | VIP: " << (isVIP ? "Yes" : "No") 
-              << " | Waiting Time: " << waitingTime << std::endl;
+    cout << "ID: " << id << " | Arrival: " << arrivalTime 
+         << " | Service: " << transactionTime << " | Wait: " << waitingTime 
+         << " | VIP: " << (isVIP ? "Yes" : "No") << " | Status: " << status << endl;
 }
