bb81b862fb9196948a7360bdc6a534d4dae57b15
fahd114 <fahd.320250114@ejust.edu.eg>
2026-05-09 15:01:01 +0300

    Update Controller.cpp

--- Controller.cpp
+++ Controller.cpp
@@ -1,7 +1,7 @@
 #include "Controller.h"
 #include <SFML/System.hpp>
 
-Controller::Controller() : sim(5, 500) {
+Controller::Controller() : sim(3, 100) {
 }
 
 void Controller::initializeSystem() {
@@ -9,20 +9,15 @@ void Controller::initializeSystem() {
 
 void Controller::startSimulation() {
     sf::Clock clock;
-    float tickRate = 0.5f;   
-
+    float tickRate = 0.5f; 
     while (gui.isOpen()) {
-        
         gui.handleEvents(); 
-
         if (sim.getIsRunning()) {
-        
             if (clock.getElapsedTime().asSeconds() >= tickRate) {
                 updateSystem();
                 clock.restart(); 
             }
         }
-
         gui.render(sim); 
     }
 }
