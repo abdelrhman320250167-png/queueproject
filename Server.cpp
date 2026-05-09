1b2527166f1c544435b39ab3dcfdec38da344e26
fahd114 <fahd.320250114@ejust.edu.eg>
2026-05-09 14:58:51 +0300

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,6 +1,8 @@
 #include "Server.h"
 #include <iostream>
 
+using namespace std;
+
 Server::Server() {
     currentCustomer = nullptr;
     busy = false;
@@ -8,25 +10,20 @@ Server::Server() {
     servedCount = 0;
 }
 
-bool Server::isFree() const {
-    return !busy;
-}
-
-bool Server::isBusy() const {
-    return busy;
-}
+bool Server::isFree() const { return !busy; }
+bool Server::isBusy() const { return busy; }
 
 void Server::assignCustomer(Customer* c) {
     currentCustomer = c;
     busy = true;
     remainingTime = c->getTransactionTime();
-    currentCustomer->setStatus(SERVING);
+    c->setStatus(SERVING);
 }
 
 void Server::updateTime() {
-    if (busy && remainingTime > 0) {
+    if (busy && currentCustomer != nullptr) {
         remainingTime--;
-        if (remainingTime == 0) {
+        if (remainingTime <= 0) {
             finishService();
         }
     }
@@ -36,28 +33,16 @@ void Server::finishService() {
     if (currentCustomer != nullptr) {
         currentCustomer->setStatus(DONE);
         currentCustomer = nullptr;
+        busy = false;
+        servedCount++;
     }
-    busy = false;
-    servedCount++;
 }
 
-Customer* Server::getCurrentCustomer() const {
-    return currentCustomer;
-}
-
-int Server::getRemainingTime() const {
-    return remainingTime;
-}
-
-int Server::getTotalServedCount() const {
-    return servedCount;
-}
+Customer* Server::getCurrentCustomer() const { return currentCustomer; }
+int Server::getRemainingTime() const { return remainingTime; }
+int Server::getTotalServedCount() const { return servedCount; }
 
 void Server::printStatus() const {
-    if (busy) {
-        std::cout << "Server is BUSY with Customer " << currentCustomer->getId() 
-                  << " | Remaining Time: " << remainingTime << std::endl;
-    } else {
-        std::cout << "Server is FREE" << std::endl;
-    }
+    if (busy) cout << "Busy (" << remainingTime << " left)";
+    else cout << "Free";
 }
